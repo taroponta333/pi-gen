9fc8eed35b96287ec36301756ffc6c6b2b89391c
taroponta333 <taroponta.333@gmail.com>
2026-06-18 20:02:03 +0900

    Update 01-run-chroot.sh

--- stage2/05-custom-apps/01-run-chroot.sh
+++ stage2/05-custom-apps/01-run-chroot.sh
@@ -2,16 +2,16 @@
 
 # 1. パッケージリストを更新し、鍵の追加に必要なツールを入れる
 apt-get update
-apt-get install -y --no-install-recommends gnupg wget
+apt-get install -y --no-install-recommends gnupg wget ca-certificates
 
 # 2. TurboWarp公式リポジトリのセキュリティ鍵と、ダウンロード先を登録
 wget https://desktop.turbowarp.org/release-signing-key.gpg -qO- | gpg --dearmor > /usr/share/keyrings/turbowarp.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/turbowarp.gpg] https://releases.turbowarp.org/deb stable main" > /etc/apt/sources.list.d/turbowarp.list
 
-# 3. 追加したTurboWarpを含めて、もう一度パッケージリストを更新
+# 3. 追加したリポジトリを含めて、もう一度パッケージリストを更新
 apt-get update
 
-# 4. デスクトップ環境、VS Code、TurboWarp、必須ツールを一括インストール
+# 4. デスクトップ環境、VS Code、TurboWarp、日本語フォントを一括インストール！
 apt-get install -y --no-install-recommends \
     lxqt-core \
     openbox \
