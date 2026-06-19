cac59b1f91b7aca071f77324c62aa515d2e1a5f2
taroponta333 <taroponta.333@gmail.com>
2026-06-19 16:17:55 +0900

    Update 00-run.sh

--- stage4/01-disable-wayvnc/00-run.sh
+++ stage4/01-disable-wayvnc/00-run.sh
@@ -2,4 +2,20 @@
 
 on_chroot <<- EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_vnc 1
+# Fcitx5をデスクトップ起動時に自動実行する設定を追加
+mkdir -p "/etc/xdg/autostart"
+cat << 'EOF' > "/etc/xdg/autostart/fcitx5.desktop"
+[Desktop Entry]
+Name=Fcitx 5
+GenericName=Input Method
+Comment=Start Input Method
+Exec=fcitx5 -d
+Icon=fcitx
+Terminal=false
+Type=Application
+Categories=System;Utility;
+StartupNotify=false
+X-GNOME-Autostart-Phase=Applications
+X-GNOME-Autostart-Delay=2
+X-KDE-autostart-phase=1
 EOF
